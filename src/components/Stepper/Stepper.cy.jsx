9002935d7f7f369779f50d68054823ee7bb42223
matheusoliv23 <matheushenri1996@gmail.com>
2022-07-20 13:46:32 -0300

    feate Introduction to cypress

---
+++ src/components/Stepper/Stepper.cy.jsx
@@ -0,0 +1,43 @@
+import Stepper from "./index";
+
+// Set up some constants for the selectors
+const counterSelector = "[data-cy=counter]";
+const incrementSelector = "[aria-label=increment]";
+const decrementSelector = "[aria-label=decrement]";
+
+describe("<Stepper>", () => {
+  it("mounts", () => {
+    cy.mount(<Stepper />);
+  });
+
+  it("deve ter como valor inicial o 0", () => {
+    cy.mount(<Stepper />);
+    cy.get(counterSelector).should("have.text", "0");
+  });
+
+  it("deve ter uma prop inicial para atribuir um valor", () => {
+    cy.mount(<Stepper initial={500} />);
+    cy.get(counterSelector).should("have.text", "500");
+  });
+
+  it("ao clicar no botão +, o contador incrementa um valor", () => {
+    cy.mount(<Stepper />);
+    cy.get(incrementSelector).click();
+    cy.get(counterSelector).should("have.text", "1");
+  });
+
+  it("ao clicar no botão -, o contador diminui um valor", () => {
+    cy.mount(<Stepper />);
+    cy.get(decrementSelector).click();
+    cy.get(counterSelector).should("have.text", "-1");
+  });
+
+  it("ao clicar em diminuir ou acrescentar os valores são mudados conforme espera-se", () => {
+    cy.mount(<Stepper initial={100} />);
+    cy.get(counterSelector).should("have.text", "100");
+    cy.get(incrementSelector).click();
+    cy.get(counterSelector).should("have.text", "101");
+    cy.get(decrementSelector).click().click();
+    cy.get(counterSelector).should("have.text", "99");
+  });
+});
